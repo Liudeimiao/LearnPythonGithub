4e9a2fdec720b41e07cbab4184193e2387ec5732
liudeimiao <840792862@qq.com>
2019-02-26 15:00:52 +0800

    变量的使用

---
+++ 06/network_bandwidth_cp.py
@@ -0,0 +1,16 @@
+# # 网络带宽计算
+# # print(100/8)
+#
+# bandwidth = 100
+# ratio = 8
+#
+# print(bandwidth/ratio)
+#
+# #bandWidth
+# #BandWidth
+# #band_width
+
+bandwidth = 100
+ratio = 9
+
+print(bandwidth/ratio)
