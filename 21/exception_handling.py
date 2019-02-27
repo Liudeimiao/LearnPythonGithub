58aa73937e12d1370e2dcedf0d821472eae151b8
liudeimiao <840792862@qq.com>
2019-02-27 15:24:50 +0800

    异常处理

--- 21/exception_handling.py
+++ 21/exception_handling.py
@@ -1,47 +1,62 @@
-# i = j
 
-
-# print())
 
 
 # a='123'
 # print(a[3])
-#
+
 # d = {'a':1 ,'b':2}
 # print(d['c'])
 
-#
+
 # year = int(input('input year:'))
 
 # try:
 #     year = int(input('input year:'))
 # except ValueError:
 #     print('年份要输入数字')
-#
-# a=123
+
+a=123
 # a.append()
 
+# try:
+#     a.append('a')
+# except AttributeError:
+#     print('int 没有此方法')
+
+
+# try:
+#     year = int(input('input yeer:'))
+#
+# except ValueError:
+#     print('年份要输入数字')
+
+# 捕获多个异常
 # except (ValueError, AttributeError, KeyError)
 
-#
+
 # try:
 #     print(1/'a')
 # except Exception as e:
 #     print(' %s' %e)
-#
+
+# raise 相当于 throw
 # try:
 #     raise NameError('helloError')
 # except NameError:
 #     print('my custom error')
 
-
-try:
-    a = open('name.txt')
-except Exception as e:
-    print(e)
-
-finally:
-    a.close()
-
+# try:
+#     a = open('name.txt')
+# except Exception as e:
+#     print(e)
+#
+# finally:
+#     a.close()
 
 
+
+# division by zero
+# try:
+#     c = 1/0
+# except Exception as  e:
+#     print(e)
